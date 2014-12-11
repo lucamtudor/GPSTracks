0baa550c2279847501b700ee91c4e7aa0e6bf43b
Tudor Luca <luca.mtudor@gmail.com>
2014-12-12 01:10:33 +0200

    Adds android post draft.

--- android/src/main/java/ro/tudorluca/gpstracks/android/MainActivity.java
+++ android/src/main/java/ro/tudorluca/gpstracks/android/MainActivity.java
@@ -2,8 +2,24 @@ package ro.tudorluca.gpstracks.android;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Date;
+
+import ro.tudorluca.gpstracks.webapi.core.Position;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +27,46 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.post_location).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                postLocation();
+            }
+        });
+    }
+
+    private void postLocation() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        String url = "http://localhost:8080/position";
+
+        Position position = new Position();
+        position.setDate(new Date());
+        position.setLatitude("12.432");
+        position.setLongitude("43.23");
+        position.setUserId(1);
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            JSONObject jsonObject = new JSONObject(mapper.writeValueAsString(position));
+            JsonObjectRequest positionRequest = new JsonObjectRequest(Request.Method.POST, url, jsonObject, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    Log.d("Position Response", response.toString());
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("Position Error", error.getMessage());
+                }
+            });
+
+            queue.add(positionRequest);
+        } catch (JSONException | JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
     }
 }
